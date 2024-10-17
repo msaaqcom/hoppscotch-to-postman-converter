e267397cb7cf50aec3d8566835c321bcae1a29ff
Hussam Abd <Hussam3bd@gmail.com>
2024-10-17 12:54:23 +0300

    map `hoppscotch_exported_files` dir

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
+import os
 
 from hoppscotch_to_postman_converter import (
     convert_hoppscotch_to_postman_collection_v21,
@@ -6,10 +6,12 @@ from hoppscotch_to_postman_converter import (
 )
 
 # Convert to Postman v2.1
-convert_hoppscotch_to_postman_collection_v21('hoppscotch_exported_file.json')
+# convert_hoppscotch_to_postman_collection_v21('hoppscotch_exported_file.json')
 
 
 # Convert to Postman environment
-convert_hoppscotch_env_to_postman_env('hoppscotch_environment.json')
 
-
+# map hoppscotch_exported_files folder to convert each file end with .json to postman collection
+for file in os.listdir('hoppscotch_exported_files'):
+    if file.endswith('.json'):
+        convert_hoppscotch_env_to_postman_env(f'hoppscotch_exported_files/{file}')
